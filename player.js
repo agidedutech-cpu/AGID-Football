c17f135be6ba49cc2e384de89991560fc0a3af01
agidedutech-cpu <agidedutech@gmail.com>
2026-07-08 05:22:35 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,21 @@
+(function () {
 
+"use strict";
+
+window.AGID_PLAYER={
+
+init:function(config){
+
+this.config=config;
+
+this.loadMatch();
+
+this.createButtons();
+
+this.activateTabs();
+
+}
+
+};
+
+})();
