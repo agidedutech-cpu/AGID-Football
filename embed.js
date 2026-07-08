1aa8ba04ff6d6dc8ad1f6023c0bd574d800d3059
agidedutech-cpu <agidedutech@gmail.com>
2026-07-08 05:33:05 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,27 +1,104 @@
 /*!
+ * ======================================================
  * AGID Football Framework
- * Version: 1.0.0
- * Author: AGID EDU TECH
+ * Version : 1.0.0
+ * Author  : AGID EDU TECH
+ * ======================================================
  */
 
-(function (window) {
+(function (window, document) {
 
-"use strict";
+    "use strict";
 
-const AGID = {
+    const AGID = {
 
-version: "1.0.0",
+        version: "1.0.0",
 
-init: function(config){
+        config: null,
 
-console.log("Framework Started");
+        container: null,
 
-console.log(config);
+        init(config) {
+
+            if (!config) {
+                throw new Error("AGID: Configuration object is required.");
+            }
+
+            this.config = config;
+
+            this.container = document.getElementById(
+                config.container || "agid-player"
+            );
+
+            if (!this.container) {
+                throw new Error(
+                    "AGID: Container element not found."
+                );
+            }
+
+            console.log("AGID Football v" + this.version);
+
+            this.loadCSS();
+
+        }
+loadCSS() {
+
+    const css = document.createElement("link");
+
+    css.rel = "stylesheet";
+
+    css.href = this.config.base +
+
+        "player.css";
+
+    css.onload = () => {
+
+        this.loadHTML();
+
+    };
+
+    document.head.appendChild(css);
+
+},
+
+      async loadHTML() {
+
+    try {
+
+        const response = await fetch(
+
+            this.config.base +
+
+            "player.html"
+
+        );
+
+        const html = await response.text();
+
+        this.container.innerHTML = html;
+
+        this.render();
+
+    }
+
+    catch (error) {
+
+        this.container.innerHTML =
+
+            "<h2>Unable to load player.</h2>";
+
+        console.error(error);
+
+    }
+
+},
+      render() {
+
+    console.log("Player Loaded");
 
 }
+    };
 
-};
+    window.AGID = AGID;
 
-window.AGID = AGID;
-
-})(window);
+})(window, document);
