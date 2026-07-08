3f723388b59212041c162a07a260c960588fa9b6
agidedutech-cpu <agidedutech@gmail.com>
2026-07-08 04:26:37 +0100

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -4,3 +4,27 @@
 // ===============================
 
 console.log("AGID Loader Started");
+if(typeof matchConfig==="undefined"){
+
+alert("Match configuration not found.");
+
+throw new Error("matchConfig is missing.");
+
+}
+
+const wrapper=document.getElementById("agid-player");
+
+if(!wrapper){
+
+throw new Error("Player container missing.");
+
+}
+wrapper.innerHTML=`
+
+<div class="loading">
+
+Loading Match...
+
+</div>
+
+`;
