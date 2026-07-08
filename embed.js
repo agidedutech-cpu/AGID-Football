01994f76e4f9e515fd52a87a63c7e848364f5733
agidedutech-cpu <agidedutech@gmail.com>
2026-07-08 05:08:49 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,1 +1,14 @@
+/*!
+ * AGID Football Framework
+ * Version: 1.0.0
+ * Author: AGID EDU TECH
+ */
 
+(function () {
+    "use strict";
+
+    console.log("=================================");
+    console.log(" AGID Football Framework v1.0 ");
+    console.log("=================================");
+
+})();
