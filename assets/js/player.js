a3083fa879f86ca340d450eb1141ca938bb93f98
agidedutech-cpu <agidedutech@gmail.com>
2026-07-08 04:30:18 +0100

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,1 +1,31 @@
-console.log("Player Loaded");
+const {
+
+    matchId,
+
+    momentumId,
+
+    competition,
+
+    teams,
+
+    streams
+
+} = matchConfig;
+
+const matchTitle =
+`${competition}: ${teams[0].name} vs ${teams[1].name}`;
+
+document.getElementById("match-title").textContent =
+matchTitle;
+
+document.getElementById("match-header").src =
+`https://www.scorebat.com/embed/matchview/${matchId}/`;
+
+document.getElementById("tv-iframe").src =
+streams[0];
+
+document.getElementById("lineup-iframe").src =
+`https://www.scorebat.com/embed/line-up/${matchId}/`;
+
+document.getElementById("events-iframe").src =
+`https://www.scorebat.com/embed/matchview/${matchId}/`;
